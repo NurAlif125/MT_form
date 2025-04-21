b1a87c9afdc7d4d6c2d8fc648d38967df95f2e2e
isalmahmood <isadv0011@gmail.com>
2025-04-21 11:33:27 +0700

    delete logo, update connection2

--- src/main/java/com/vensys/appcm/dbase/DBconnection2.java
+++ src/main/java/com/vensys/appcm/dbase/DBconnection2.java
@@ -38,14 +38,14 @@ public class DBconnection2 {
             if (inputStream != null) {
                 prop.load(inputStream);
                 String DRIVER = prop.getProperty("driver");
-                String URL = prop.getProperty("url");
+                String URL = prop.getProperty("url2");
 //                String USER = prop.getProperty("user");
 //                String PASS = prop.getProperty("password");
                 String USER = null;
                 String PASS = null;
                 try {
-                    USER = enc.decryptTD(prop.getProperty("user"), "AKey@VenSys");
-                    PASS = enc.decryptTD(prop.getProperty("password"), "AKey@VenSys");
+                    USER = enc.decryptTD(prop.getProperty("user2"), "AKey@VenSys");
+                    PASS = enc.decryptTD(prop.getProperty("password2"), "AKey@VenSys");
                 } catch (Exception ex) {
                     java.util.logging.Logger.getLogger(DBconnection.class.getName()).log(Level.SEVERE, null, ex);
                 }
