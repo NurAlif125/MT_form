e76a1a811aed7d14ce967dd6db86bf6fc688a1e0
unknown <putriaditya166@gmail.com>
2025-04-28 10:56:52 +0700

    fixing font freesans report transaction - XLS

--- src/main/java/com/vensys/appcm/controller/StartupScheduler.java
+++ src/main/java/com/vensys/appcm/controller/StartupScheduler.java
@@ -20,28 +20,28 @@ public class StartupScheduler implements ServletContextListener {
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-        System.out.println("Inisialisasi scheduler...");
-        System.out.println("Waktu server sekarang: " + LocalDateTime.now());
+        //System.out.println("Inisialisasi scheduler...");
+        //System.out.println("Waktu server sekarang: " + LocalDateTime.now());
 
         scheduler = Executors.newScheduledThreadPool(1);
 
         Runnable task = () -> {
             LocalTime now = LocalTime.now();
             LocalTime start = LocalTime.of(01, 00);
-            LocalTime end = LocalTime.of(01, 05);
+            LocalTime end = LocalTime.of(22, 00);
 
             if (!now.isBefore(start) && now.isBefore(end)) {
                 disableInactiveUsers();
             } else {
-                System.out.println("Di luar rentang waktu 13:50–14:00, skip eksekusi pada: " + now);
+               // System.out.println("Di luar rentang waktu 13:50–14:00, skip eksekusi pada: " + now);
             }
         };
 
         long initialDelay = 0;       // langsung mulai
-        long period = 60;             // setiap 5 detik
+        long period = 300;             // setiap 5 detik
 
         scheduler.scheduleAtFixedRate(task, initialDelay, period, TimeUnit.SECONDS);
-        System.out.println("Scheduler aktif. Akan berjalan setiap 5 detik, tetapi hanya eksekusi antara 13:50–14:00.");
+       // System.out.println("Scheduler aktif. Akan berjalan setiap 5 detik, tetapi hanya eksekusi antara 13:50–14:00.");
     }
 
     @Override
@@ -49,32 +49,30 @@ public class StartupScheduler implements ServletContextListener {
         if (scheduler != null && !scheduler.isShutdown()) {
             scheduler.shutdown();
         }
-        System.out.println("Scheduler dimatikan.");
+        //System.out.println("Scheduler dimatikan.");
     }
 
     private void disableInactiveUsers() {
         try {
-            System.out.println("[Scheduler] Eksekusi dimulai pada: " + LocalTime.now());
+            //System.out.println("[Scheduler] Eksekusi dimulai pada: " + LocalTime.now());
 
             DBconnection dbConn = new DBconnection();
             //DBconnection2 dbConn2 = new DBconnection2();
             Connection conn1 = null;
             //Connection conn2 = null;
 
-            String sql = """
-                UPDATE users
-                SET enable = 0
-                WHERE enable = 1
-                  AND last_login IS NOT NULL
-                  AND to_timestamp(last_login, 'YYYY-MM-DD HH24:MI:SS') < CURRENT_DATE - INTERVAL '1 month'';
-            """;
+            String sql = "UPDATE users\n" +
+                        "SET enable = 0\n" +
+                        "WHERE enable = 1\n" +
+                        "  AND last_login IS NOT NULL\n" +
+                        "  AND to_timestamp(last_login, 'YYYY-MM-DD HH24:MI:SS') < CURRENT_DATE - INTERVAL '1 month';";
 
             try {
                 conn1 = dbConn.getConnection();
                 //conn2 = dbConn2.getConnection2();
 
                 if (conn1 == null ) {
-                    System.err.println(" Salah satu koneksi DB null.");
+                   // System.err.println(" Salah satu koneksi DB null.");
                     return;
                 }
 
@@ -85,7 +83,7 @@ public class StartupScheduler implements ServletContextListener {
                 //System.out.println("✅ DB2 - Jumlah user dinonaktifkan: " + affected2);
 
             } catch (Exception ex) {
-                System.err.println(" Error saat menjalankan update:");
+                //System.err.println(" Error saat menjalankan update:");
                 ex.printStackTrace();
             } finally {
                 dbConn.closeConnection();
@@ -93,7 +91,7 @@ public class StartupScheduler implements ServletContextListener {
             }
 
         } catch (Exception e) {
-            System.err.println(" ERROR di disableInactiveUsers()");
+            //System.err.println(" ERROR di disableInactiveUsers()");
             e.printStackTrace();
         }
     }
