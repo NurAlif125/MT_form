43817f5174aa3d3abe3cbdfe531a644b70fa0456
isalmahmood <isadv0011@gmail.com>
2025-06-20 03:18:01 +0700

    update getnow notifications

--- src/main/java/com/vensys/appcm/dbase/DBnotification.java
+++ src/main/java/com/vensys/appcm/dbase/DBnotification.java
@@ -32,7 +32,7 @@ public class DBnotification {
         int data = 0;
         String sql = "select count(id_notif) as count from notifications \n" +
                     "WHERE ? = ANY(string_to_array(COALESCE(msg_to_role, ''), ','))\n" +
-                    "AND NOT (? = ANY(string_to_array(COALESCE(is_read_userid, ''), ',')));";
+                    "AND NOT (? = ANY(string_to_array(COALESCE(is_read_userid, ''), ','))) AND TO_CHAR(created_at, 'YYYY-MM-DD') = TO_CHAR(now(), 'YYYY-MM-DD');";
 
         try (PreparedStatement st = this.conn.prepareStatement(sql)) {  // Auto-close PreparedStatement
             st.setString(1, roleId); 
@@ -52,7 +52,7 @@ public class DBnotification {
         String sql = "SELECT id_notif, title_msg, msg_body, TO_CHAR(created_at, 'YYYY-MM-DD HH24:MI:SS') as created_at FROM notifications \n" +
                      "WHERE ? = ANY(string_to_array(COALESCE(msg_to_role, ''), ','))\n" +
                      "AND NOT (? = ANY(string_to_array(COALESCE(is_read_userid, ''), ',')))\n" +
-                     "ORDER BY created_at DESC;";
+                     " TO_CHAR(created_at, 'YYYY-MM-DD') = TO_CHAR(now(), 'YYYY-MM-DD') ORDER BY created_at DESC;";
 
         try (PreparedStatement st = this.conn.prepareStatement(sql)) {
             st.setString(1, roleId);
