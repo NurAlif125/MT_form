289d73603d1459b19b1e5cca933a42357fed2160
Nur Alif <2310512125@mahasiswa.upnvj.ac.id>
2025-10-07 16:09:42 +0700

    refactor mt767.properties

--- src/main/webapp/Category7/attribute/AMT767.java
+++ src/main/webapp/Category7/attribute/AMT767.java
@@ -15,9 +15,8 @@ import java.util.Properties;
  * @author mslam
  */
 public class AMT767 {
-
     Logger log = Logger.getLogger(AMT767.class);
-
+    
     public MT767 getAtributeMT767() {
         MT767 data = new MT767();
         Properties prop = new Properties();
@@ -26,7 +25,7 @@ public class AMT767 {
         } catch (IOException ex) {
             log.error("getAtributeMT767():" + ex.toString());
         }
-
+        
         data.setMf15a(prop.getProperty("mf15a"));
         data.setMf27(prop.getProperty("mf27"));
         data.setMf21(prop.getProperty("mf21"));
@@ -34,6 +33,7 @@ public class AMT767 {
         data.setOf23s(prop.getProperty("of23s"));
         data.setOf72z(prop.getProperty("of72z"));
         data.setOf23x(prop.getProperty("of23x"));
+        
         data.setMf15b(prop.getProperty("mf15b"));
         data.setMf20(prop.getProperty("mf20"));
         data.setMf26e(prop.getProperty("mf26e"));
@@ -49,16 +49,17 @@ public class AMT767 {
         data.setOf77u(prop.getProperty("of77u"));
         data.setOf24e(prop.getProperty("of24e"));
         data.setOf24g(prop.getProperty("of24g"));
+        
         data.setMf15c(prop.getProperty("mf15c"));
-        data.setOf32b2(prop.getProperty("of32b"));
-        data.setOf33b2(prop.getProperty("of33b"));
-        data.setOf23b2(prop.getProperty("of23b"));
-        data.setOf31e2(prop.getProperty("of31e"));
-        data.setOf35g2(prop.getProperty("of35g"));
+        data.setOf32b_c(prop.getProperty("of32b_c"));
+        data.setOf33b_c(prop.getProperty("of33b_c"));
+        data.setOf23b_c(prop.getProperty("of23b_c"));
+        data.setOf31e_c(prop.getProperty("of31e_c"));
+        data.setOf35g_c(prop.getProperty("of35g_c"));
         data.setOf59(prop.getProperty("of59"));
         data.setOf77l(prop.getProperty("of77l"));
-        data.setOf24e2(prop.getProperty("of24e"));
-        data.setOf24g2(prop.getProperty("of24g"));
+        data.setOf24e_c(prop.getProperty("of24e_c"));
+        data.setOf24g_c(prop.getProperty("of24g_c"));
         return data;
     }
 }
--- src/main/webapp/Category7/controller/TagMT767.java
+++ src/main/webapp/Category7/controller/TagMT767.java
@@ -12,6 +12,10 @@ public class TagMT767 {
     MT767 mt767 = new MT767();
     AMT767 attributeMT767 = new AMT767();
     CHeader ch;
+    
+    // Flag untuk tracking sequence
+    private boolean inSequenceB = false;
+    private boolean inSequenceC = false;
 
     public TagMT767(CHeader ch) {
         this.ch = ch;
@@ -31,68 +35,115 @@ public class TagMT767 {
             if (tagName.equalsIgnoreCase("15A")) {
                 tags = mt767.getMf15a();
                 this.ch.readBlock4(tags, tagName, tagValue);
-            } else if (tagName.equalsIgnoreCase("20")) {
-                tags = mt767.getMf20();
+                inSequenceB = false;
+                inSequenceC = false;
+            } else if (tagName.equalsIgnoreCase("27")) {
+                tags = mt767.getMf27();
                 this.ch.readBlock4(tags, tagName, tagValue);
             } else if (tagName.equalsIgnoreCase("21")) {
                 tags = mt767.getMf21();
                 this.ch.readBlock4(tags, tagName, tagValue);
-            } else if (tagName.equalsIgnoreCase("23B")) {
-                tags = mt767.getMf23b();
+            } else if (tagName.equalsIgnoreCase("22A")) {
+                tags = mt767.getMf22a();
+                this.ch.readBlock4(tags, tagName, tagValue);
+            } else if (tagName.equalsIgnoreCase("23S")) {
+                tags = mt767.getOf23s();
+                this.ch.readBlock4(tags, tagName, tagValue);
+            } else if (tagName.equalsIgnoreCase("72Z")) {
+                tags = mt767.getOf72z();
+                this.ch.readBlock4(tags, tagName, tagValue);
+            } else if (tagName.equalsIgnoreCase("23X")) {
+                tags = mt767.getOf23x();
                 this.ch.readBlock4(tags, tagName, tagValue);
             }
             else if (tagName.equalsIgnoreCase("15B")) {
                 tags = mt767.getMf15b();
                 this.ch.readBlock4(tags, tagName, tagValue);
+                inSequenceB = true;
+                inSequenceC = false;
+            } else if (tagName.equalsIgnoreCase("20")) {
+                tags = mt767.getMf20();
+                this.ch.readBlock4(tags, tagName, tagValue);
+            } else if (tagName.equalsIgnoreCase("26E")) {
+                tags = mt767.getMf26e();
+                this.ch.readBlock4(tags, tagName, tagValue);
             } else if (tagName.equalsIgnoreCase("30")) {
                 tags = mt767.getMf30();
                 this.ch.readBlock4(tags, tagName, tagValue);
+            } else if (tagName.equalsIgnoreCase("52A") || tagName.equalsIgnoreCase("52D")) {
+                tags = mt767.getMf52a();
+                this.ch.readBlock4(tags, tagName, tagValue);
+            } else if (tagName.equalsIgnoreCase("23")) {
+                tags = mt767.getOf23();
+                this.ch.readBlock4(tags, tagName, tagValue);
             } else if (tagName.equalsIgnoreCase("32B")) {
-                tags = mt767.getOf32b();
+                if (inSequenceC) {
+                    tags = mt767.getOf32b_c();
+                } else {
+                    tags = mt767.getOf32b();
+                }
                 this.ch.splitMT32B(tags, tagName, tagValue);
             } else if (tagName.equalsIgnoreCase("33B")) {
-                tags = mt767.getOf33b();
+                if (inSequenceC) {
+                    tags = mt767.getOf33b_c();
+                } else {
+                    tags = mt767.getOf33b();
+                }
                 this.ch.splitMT32B(tags, tagName, tagValue);
-            } else if (tagName.equalsIgnoreCase("71B")) {
-                tags = mt767.getOf71b();
+            } else if (tagName.equalsIgnoreCase("23B")) {
+                if (inSequenceC) {
+                    tags = mt767.getOf23b_c();
+                } else {
+                    tags = mt767.getOf23b();
+                }
                 this.ch.readBlock4(tags, tagName, tagValue);
-            } else if (tagName.equalsIgnoreCase("34B")) {
-                tags = mt767.getOf34b();
-                this.ch.splitMT32B(tags, tagName, tagValue);
-            } else if (tagName.equalsIgnoreCase("39A")) {
-                tags = mt767.getOf39a();
+            } else if (tagName.equalsIgnoreCase("31E")) {
+                if (inSequenceC) {
+                    tags = mt767.getOf31e_c();
+                } else {
+                    tags = mt767.getOf31e();
+                }
+                this.ch.readBlock4(tags, tagName, tagValue);
+            } else if (tagName.equalsIgnoreCase("35G")) {
+                if (inSequenceC) {
+                    tags = mt767.getOf35g_c();
+                } else {
+                    tags = mt767.getOf35g();
+                }
+                this.ch.readBlock4(tags, tagName, tagValue);
+            } else if (tagName.equalsIgnoreCase("59A") || tagName.equalsIgnoreCase("59")) {
+                if (inSequenceC) {
+                    tags = mt767.getOf59();
+                    this.ch.splitRowData(tags, tagName, tagValue);
+                } else {
+                    tags = mt767.getOf59a();
+                    this.ch.readBlock4(tags, tagName, tagValue);
+                }
+            } else if (tagName.equalsIgnoreCase("77U")) {
+                tags = mt767.getOf77u();
+                this.ch.readBlock4(tags, tagName, tagValue);
+            } else if (tagName.equalsIgnoreCase("24E")) {
+                if (inSequenceC) {
+                    tags = mt767.getOf24e_c();
+                } else {
+                    tags = mt767.getOf24e();
+                }
+                this.ch.readBlock4(tags, tagName, tagValue);
+            } else if (tagName.equalsIgnoreCase("24G")) {
+                if (inSequenceC) {
+                    tags = mt767.getOf24g_c();
+                } else {
+                    tags = mt767.getOf24g();
+                }
                 this.ch.readBlock4(tags, tagName, tagValue);
             }
             else if (tagName.equalsIgnoreCase("15C")) {
                 tags = mt767.getMf15c();
                 this.ch.readBlock4(tags, tagName, tagValue);
-            }
-            else if (tagName.equalsIgnoreCase("32B")) {
-                tags = mt767.getOf32b2();
-                this.ch.splitMT32B(tags, tagName, tagValue);
-            } else if (tagName.equalsIgnoreCase("33B")) {
-                tags = mt767.getOf33b2();
-                this.ch.splitMT32B(tags, tagName, tagValue);
-            } else if (tagName.equalsIgnoreCase("23B")) {
-                tags = mt767.getOf23b2();
-                this.ch.readBlock4(tags, tagName, tagValue);
-            } else if (tagName.equalsIgnoreCase("31E")) {
-                tags = mt767.getOf31e2();
-                this.ch.readBlock4(tags, tagName, tagValue);
-            } else if (tagName.equalsIgnoreCase("35G")) {
-                tags = mt767.getOf35g2();
-                this.ch.readBlock4(tags, tagName, tagValue);
-            } else if (tagName.equalsIgnoreCase("59")) {
-                tags = mt767.getOf59();
-                this.ch.splitRowData(tags, tagName, tagValue);
+                inSequenceB = false;
+                inSequenceC = true;
             } else if (tagName.equalsIgnoreCase("77L")) {
                 tags = mt767.getOf77l();
-                this.ch.readBlock4(tags, tagName, tagValue);
-            } else if (tagName.equalsIgnoreCase("24E")) {
-                tags = mt767.getOf24e2();
-                this.ch.readBlock4(tags, tagName, tagValue);
-            } else if (tagName.equalsIgnoreCase("24G")) {
-                tags = mt767.getOf24g2();
                 this.ch.readBlock4(tags, tagName, tagValue);
             }
         }
